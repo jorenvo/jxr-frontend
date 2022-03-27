d986081f7f35759a6802c232ac0efeecd7605382
Joren Van Onder <jov@odoo.com>
2022-03-26 21:29:13 -0700

    Replace test indexed code copy with something more robust

--- post_build.sh
+++ post_build.sh
@@ -13,6 +13,8 @@ echo "Replacing JXR_BACKEND with ${JXR_BACKEND}...  "
 # Can't double quote ${...} because: echo "\" => \\
 sed -i.bak s/JXR_BACKEND/${JXR_BACKEND//\//\\\/}/g dist/js/*.js
 
-echo "Copying some test indexed code..."
-mkdir dist/jxr-code
-cp -r ~/Code/Aerial dist/jxr-code/
+if [ "${JXR_BACKEND}" = "http://localhost:8081" ]; then
+    echo "Copying some test indexed code..."
+    mkdir dist/jxr-code
+    cp -r ~/Code/jxr-backend dist/jxr-code
+fi
